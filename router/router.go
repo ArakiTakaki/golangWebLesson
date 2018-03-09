803ec1cdf0cfa071db4bd8a1b07e75db24c33259
ArakiTakaki <sjyyj008@gmail.com>
2018-03-09 23:39:54 +0900

    プロジェクト始動

---
+++ router/router.go
@@ -0,0 +1,23 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// GetRouter ルーターの設定をして、gin.Engineを返す
+func GetRouter() *gin.Engine {
+	var r = gin.Default()
+
+	// 静的ファイルを配置する場所選択
+	r.Static("/js", "./public/js")
+	r.Static("/css", "./public/css")
+	r.Static("/image", "./public/image")
+
+	r.LoadHTMLGlob("view/*")
+	r.NoRoute(func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+	return r
+}
